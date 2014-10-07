b96232ac0526e44eabb715e4271c3cac06136d80
Wendy Dherin <wendydherin@gmail.com>
2014-10-06 20:57:29 -0700

    Added code to remove strings made up of only digits, and to remove empty strings.

--- exercise07/wordcount.py
+++ exercise07/wordcount.py
@@ -17,9 +17,13 @@ def main():
 
 		# build histogram
 		for item in line:
+			# chuck strings of digits
+			if item.isdigit() or item == "":
+				continue
 			hist[item] = hist.get(item, 0) + 1
 	
 	hist_items = hist.items()
+	hist_items.sort(reverse=True)
 	print hist_items
 
 if __name__ == "__main__":
