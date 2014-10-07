800c52e6a27bde8809af2f07bd33eeaf901ba6a0
Wendy Dherin <wendydherin@gmail.com>
2014-10-06 21:57:17 -0700

    Iterate over list of tuples, sorting words and printing.

--- exercise07/wordcount.py
+++ exercise07/wordcount.py
@@ -22,12 +22,22 @@ def main():
 				continue
 			hist[item] = hist.get(item, 0) + 1
 	
-	# invert histogram
+	# invert histogram, so key = frequency and value = list of words
 	inverse = {}
 	for key in hist:
 		val = hist[key]
 		inverse.setdefault(val,[]).append(key)
-	print inverse
+
+	# convert inverted histo into list of tuples sorted by descending frequency
+	inverse_pairs = sorted(inverse.items(), reverse=True)
+	
+	# loop through list of tuples, sorting words for each one & printing
+	for pair in inverse_pairs:
+		words = sorted(pair[1])
+		for word in words:
+			print "%s: %r" % (word, pair[0])
+
+
 
 if __name__ == "__main__":
 	main()
