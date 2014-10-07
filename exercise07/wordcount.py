e0a28f048ebd42207a4046f222ff7ad35271c23a
Wendy Dherin <wendydherin@gmail.com>
2014-10-06 20:23:22 -0700

    Added dunder main.

--- exercise07/wordcount.py
+++ exercise07/wordcount.py
@@ -2,11 +2,15 @@ from sys import argv
 
 script, filename = argv
 
-hist = {}
+def main():
+	hist = {}
 
-fin = open(filename)
-for line in fin:
-	line = line.rstrip().split(" ")
-	for item in line:
-		hist[item] = hist.get(item, 0) + 1
-print hist
+	fin = open(filename)
+	for line in fin:
+		line = line.rstrip().split(" ")
+		for item in line:
+			hist[item] = hist.get(item, 0) + 1
+	print hist
+
+if __name__ == "__main__":
+	main()
